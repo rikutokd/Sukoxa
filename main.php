7576e78ec590d6fff2a053f84d8d13c05f5fb3be
rikutokd <rikutokd@yahoo.co.jp>
2022-12-24 19:53:13 +0900

    [update]emoji機能追加

--- main.php
+++ main.php
@@ -5,20 +5,25 @@ include __DIR__.'/vendor/autoload.php';
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Discord\Parts\Channel\Channel;
+use Discord\Parts\Channel\Webhook;
 use Discord\Parts\User\Member;
+use Discord\Parts\Guild\Guild;
 use Discord\WebSockets\Intents;
 use Discord\WebSockets\Event;
+use Discord\Helpers\Collection;
+use Discord\Repository\Channel\WebhookRepository;
 
-use sukoxa\DiscordUtil;
 
 // .envを使用する
 Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
 $tokenID = $_ENV['tokenID'];
+$myGuildID = $_ENV['myGuildID'];
 
 $discord = new \Discord\Discord([
     'token' => $tokenID,
-    'intents' => Intents::getDefaultIntents() | Intents::MESSAGE_CONTENT // Required to get message content, enable it on https://discord.com/developers/applications/
+    'intents' => Intents::getDefaultIntents() | Intents::MESSAGE_CONTENT | Intents::GUILD_MEMBERS,
+    'loadAllMembers' => true
 ]);
 
 $discord->on('ready', function (Discord $discord) {
@@ -27,10 +32,27 @@ $discord->on('ready', function (Discord $discord) {
     // Listen for messages
     $discord->on('message', function (Message $message, Discord $discord) {
 
-        // If message is "ping"
-        if ($message->content == 'ping') {
-            // Reply with "pong"
-            $message->reply('pong');
+
+        if ($message->content == '!sバトルヒーリングスキル') {
+            $message->reply('バトルヒーリングスキルによる自動回復が10秒で600ポイントある。'.
+            '何時間攻撃しても俺は倒せないよ。');
+        }
+
+        if ($message->content == '!sCommand') {
+            $message->reply('このコマンドが使えるよ：!sおみくじ, !sバトルヒーリングスキル, システムコールコマンドリスト, システムコールジェネレートイルミネーション');
+        }
+
+        if ($message->content == 'システムコールジェネレートイルミネーション') {
+            $channel_id = $message->channel->id;
+            $guild = $message->guild;
+            $channel = $guild->channels->get('id', $channel_id);
+            $channel->sendMessage(':dizzy: :dizzy: :dizzy: :dizzy: :dizzy:'. "\n".
+            ':dizzy:  '. $message->member->user. '  :dizzy:'. "\n".
+            ':dizzy: :dizzy: :dizzy: :dizzy: :dizzy:');
+        }
+
+        if ($message->content == 'システムコールコマンドリスト') {
+            $message->reply('このコマンドが使えるよ：!sおみくじ, !sバトルヒーリングスキル, システムコールコマンドリスト, システムコールジェネレートイルミネーション');
         }
 
         if ($message->content == '!shutdown') {
@@ -97,11 +119,11 @@ $discord->on('ready', function (Discord $discord) {
         }
 
         if ($message->content == '!sDebug') {
-            try {
-                
-            } catch (\Throwable $th) {
-                echo 'エラー';
-            }
+                $guild = $discord->guilds->get("id", $myGuildID);
+                $members = $guild->members;
+                print_r($members);
+                $channels = $guild->channels;
+                print_r($channels);
 
         }
 
